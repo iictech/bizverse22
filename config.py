6a1f393c419f70526f54cacca5f5206fad715579
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-04-06 01:33:49 +0530

    Initial Commit

---
+++ config.py
@@ -0,0 +1,9 @@
+config = {
+  "apiKey": "AIzaSyAEluVyKeI4U_LtM0UTJGb28KVrvYSODMY",
+  "authDomain": "bizverse-2k22.firebaseapp.com",
+  "projectId": "bizverse-2k22",
+  "storageBucket": "bizverse-2k22.appspot.com",
+  "messagingSenderId": "459582380513",
+  "appId": "1:459582380513:web:b5707192b56813c87b5aa0",
+  "measurementId": "G-Q0W66T04SV"
+}
---
+++ main.py
@@ -0,0 +1,6 @@
+from firebase import Firebase
+from config import config
+
+
+firebase = Firebase(config)
+
