ac32ebbdf60f401baa5c4403ba8e9b22da12b1ab
priyasu-cx <codexposer.priyasu25@gmail.com>
2022-04-09 01:00:59 +0530

    razorpay payment gateway

--- README.md
+++
@@ -1,9 +0,0 @@
-# DO NOT MAKE THIS PUBLIC ( CONTAINS FIREBASE CONFIG )
-
-## URL : https://bizverse-2k22.web.app/
-
-### To Do
-- [x] Sign Up flow similar to ktj.in
-- [X] User Dashboard
-- [ ] Registration Flow
-- [ ] Redirect Flow
---
+++ backend/index.js
@@ -0,0 +1,51 @@
+const app = require('express')()
+
+const path = require('path')
+
+const cors = require('cors')
+
+const shortid = require('shortid')
+
+const Razorpay = require('razorpay')
+
+//Initialize razorpay credentials
+
+const razorpay  =new Razorpay({
+    key_id: "rzp_test_8kbWdeJfhioDsg",
+    key_secret: "4AYtdYVilRK99w2pM7SF39ru",
+});
+
+app.use(cors())
+
+app.get('/logo.jpg', (req, res)=>{
+    res.sendFile(path.join(__dirname,"logo.jpg"))
+})
+
+app.post('/razorpay', async (req, res)=>{
+    const payment_capture =1
+    const amount = 999
+    const currency = 'INR'
+
+    const options = {
+        amount: amount * 100,
+        currency: currency,
+        receipt: shortid.generate(),
+        payment_capture
+    }
+
+    try{
+        const response = await razorpay.orders.create(options)
+        console.log(response)
+        res.json({
+            id:response.id,
+            currency: response.currency,
+            amount: response.amount
+        })
+    }catch(error){
+        console.log(error)
+    }
+})
+
+app.listen(5000, ()=> {
+    console.log("App is listening on port 5000")
+})
