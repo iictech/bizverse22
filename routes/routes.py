cd0ae1730e3a900dd3bbd55bdae842b37c94267b
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-04-10 01:13:34 +0530

    mails added

--- routes/routes.py
+++ routes/routes.py
@@ -27,7 +27,7 @@ async def register(evgId: str):
     exception = HTTPException(
         status_code=status.HTTP_404_NOT_FOUND)
     body = f"""
-Greetings from Bizverse,
+Greetings from BizVerse,
 
 Your registration is now complete. We welcome you to be a part of Bizverse.
 Your evg-ID is: {evgId}. Please don't share your evgID with others.
@@ -37,9 +37,9 @@ You can now visit our website to get the latest updates on our Events, Guest lec
 Hope to see you soon in our upcoming events.
 
 Regards,
-Bizverse Team 
+BizVerse Team 
     """
-    subject = "Registration Successful for Bizverse 2022"
+    subject = "Registration Successful for BizVerse 2022"
     
     if not isValidId(evgId):
         raise exception
@@ -53,20 +53,33 @@ Bizverse Team
 async def brain_it_out(evgId: str):
     exception = HTTPException(
         status_code=status.HTTP_404_NOT_FOUND)
+    try:
+        name, mail = await getUserNameAndMail(evgId)
+    except:
+        raise exception
     
-    subject = "Successfully Registered for Brain-It-Out, Bizverse 2022"
-    body = """
-Greetings from Bizverse,
+    subject = "Successfully Registered for Brain-It-Out, BizVerse 2022"
+    body = f"""
+Dear {name}
+
+Greetings from BizVerse,
+
+We have successfully received your registration for the event “Brain-It-Out”. 
+
+Brain-It-Out is a fun event where teams will be quizzed on topics such as business, technology, innovation and the market scenario of today’s world. The team with the most answers correct, will win the game. 
+
+For rules and further details, please keep checking your email. 
+
+We hope you enjoy the event!
 
 Regards,
-Bizverse Team 
+BizVerse Team
     """
 
     if not isValidId(evgId):
         raise exception
     
     try:
-        mail = await getUserMail(evgId)
         await sendMail(subject, body, mail)
     except:
         raise exception
@@ -76,19 +89,31 @@ async def ipr_workshop(evgId: str):
     exception = HTTPException(
         status_code=status.HTTP_404_NOT_FOUND)
     
-    subject = "Successfully Registered for IPR Workshop, Bizverse 2022"
-    body = """
-Greetings from Bizverse,
+    try:
+        name, mail = await getUserNameAndMail(evgId)
+    except:
+        raise exception
+    
+    subject = "Successfully Registered for IPR Workshop, BizVerse 2022"
+    body = f"""
+Dear {name}
+
+Greetings from BizVerse,
+
+We have successfully received your registration for the event “IPR Workshop”. 
+
+For rules and further details, please keep checking your email.
+
+We hope you enjoy the event!
 
 Regards,
-Bizverse Team 
+BizVerse Team
     """
 
     if not isValidId(evgId):
         raise exception
     
     try:
-        mail = await getUserMail(evgId)
         await sendMail(subject, body, mail)
     except:
         raise exception
@@ -98,19 +123,33 @@ async def logo_and_poster(evgId: str):
     exception = HTTPException(
         status_code=status.HTTP_404_NOT_FOUND)
     
-    subject = "Successfully Registered for Logo and Poster Designing, Bizverse 2022"
-    body = """
-Greetings from Bizverse,
+    try:
+        name, mail = await getUserNameAndMail(evgId)
+    except:
+        raise exception
+    
+    subject = "Successfully Registered for Logo and Poster Designing, BizVerse 2022"
+    body = f"""
+Dear {name}
+
+Greetings from BizVerse,
+
+We have successfully received your registration for the event “Logo and Poster Designing”. 
+
+Logo and Poster Designing is an event where you can showcase your creative and marketing side by designing some of the most intriguing and expressive logos and posters.
+
+For rules and further details, please keep checking your email.
+
+We hope you enjoy the event!
 
 Regards,
-Bizverse Team 
+BizVerse Team
     """
 
     if not isValidId(evgId):
         raise exception
     
     try:
-        mail = await getUserMail(evgId)
         await sendMail(subject, body, mail)
     except:
         raise exception
@@ -120,19 +159,33 @@ async def hackathon(teamEvgId: str, evgId: str):
     exception = HTTPException(
         status_code=status.HTTP_404_NOT_FOUND)
     
-    subject = "Successfully Registered for HackUrWay, Bizverse 2022"
-    body = """
-Greetings from Bizverse,
+    try:
+        name, mail = await getUserNameAndMail(evgId)
+    except:
+        raise exception
+
+    subject = "Successfully Registered for HackUrWay, BizVerse 2022"
+    body = f"""
+Dear {name}
+
+Greetings from BizVerse,
+
+We have successfully received your registration for the event “HackUrWay”. 
+
+HackUrWay is an exciting hackathon where teams will compete against each other to solve some of the relevant problems we face in our daily lives by inculcating their problem-solving skills. 
+
+For rules and further details, please keep checking your email.
+
+We hope you enjoy the event!
 
 Regards,
-Bizverse Team 
+BizVerse Team
     """
 
     if not isValidId(evgId):
         raise exception
     
     try:
-        mail = await getUserMail(evgId)
         await sendMail(subject, body, mail)
     except:
         raise exception
