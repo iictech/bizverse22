0619d41c87e10d7cf0e81f7ba6b31666d132414e
vivek26anand <heater.mitra@gmail.com>
2022-04-15 01:36:45 +0530

    footer update

--- src/Refund.js
+++ src/Refund.js
@@ -1,11 +1,17 @@
 export default function Terms(){
     return (
-        <div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
-            <h1 className="text-xl font-bold text-center p-2">Refund Policy</h1>
-              <p className="p-3 text-center">
-             Sint pariatur ipsum nulla elit ea fugiat veniam est amet est officia. Proident cillum nisi quis et consequat irure consequat mollit elit cillum voluptate officia aliqua. Duis adipisicing laborum sit incididunt aute sint ea irure mollit do ea quis. Sit aliquip reprehenderit irure nisi cillum elit sit exercitation sint. Enim duis pariatur officia ad do aliqua enim Lorem duis cillum dolore. Ut labore tempor officia minim voluptate officia. Deserunt consectetur incididunt elit nulla.
-              </p>
+        <div className="text-center flex justify-center items-center h-screen">
+            <div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6 w-1/2">
+              <p className="text-lg font-bold text-center p-2">
+              Cancellation</p>
+<p>IIC TMSL Biz Verse prohibits the cancellation of registration for any event after a payment has already been made successfully, hence we advice you to carefully review your selection of events, before confirming the payment.</p>
+<p className="text-lg font-bold text-center p-2">Refund Policy</p>
+<p>Carefully review your selection of events before you confirm the payment. Biz Verse prohibits refunds or exchanges after you have made payments for one/or more events. If you face any technical issues, IIC TMSL will refund your money via Razorpay to your payment or given bank account within 5-7 working days.</p>
+
+<p className="text-lg font-bold text-center p-2">Contact Us</p>
+<p>In case you have any suggestions or questions regarding our Refund or Cancellation Policy, please contact us at iictmsl4@gmail.com  </p>            
+</div>
+        </div>
     
-            </div>
     )
 }
