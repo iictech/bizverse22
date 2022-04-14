1ffd65df84409a4f180ac5cf56a24bdc0fb74b77
vivek26anand <heater.mitra@gmail.com>
2022-04-14 14:44:53 +0530

    footer update

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -136,6 +136,37 @@ export default function Contact() {
         </li>
       ))}
     </ul>
+    <h1 id="contact-us"className="text-center text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl pt-20 pb-20">Contact IIC</h1>
+    <div className='flex justify-center'>
+    <div className="w-1/2 col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
+          <div className='p-4 text-center'>
+          TECHNO INDIA : EM-4/1, Sector-V, <br/>
+        Salt Lake, Kolkata-700091,West Bengal
+          </div>
+          <div>
+            <div className="-mt-px flex divide-x divide-gray-200">
+              <div className="-ml-px w-0 flex-1 flex">
+                <a
+                  href=''
+                  className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
+                >
+                  <MailIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+                  <span className="ml-3">iic.tmsl@gmail.com</span>
+                </a>
+              </div>
+              <div className="-ml-px w-0 flex-1 flex">
+                <a
+                  href={`tel:+918271538524`}
+                  className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
+                >
+                  <PhoneIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+                  <span className="ml-3">+91 82715 38524</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
     </div>
     </div>
   )
